299b23557d0ba475a7e7a98d80fcb603e967568a
Krzysztof Len <krzysztof.len@POSSIBLE.com>
2017-09-28 15:50:31 +0200

    'upgrade-pwa'

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -1,16 +1,98 @@
-var shareImageButton = document.querySelector('#share-image-button');
-var createPostArea = document.querySelector('#create-post');
-var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+const shareImageButton = document.querySelector('#share-image-button');
+const createPostArea = document.querySelector('#create-post');
+const closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+const pwaContactsList = document.querySelector('#pwa-contacts-list');
 
-function openCreatePostModal() {
+const openCreatePostModal = () => {
 	createPostArea.style.display = 'block';
 	shareImageButton.style.display = 'none';
 }
 
-function closeCreatePostModal() {
+const closeCreatePostModal = () => {
 	createPostArea.style.display = 'none';
 	shareImageButton.style.display = 'block';
 }
+
+const createContact = () => {
+	for (let i = 0; i < 3; i++) {
+		
+	const cardWrapper = document.createElement('div');
+	cardWrapper.className = 'demo-card-square mdl-card mdl-shadow--2dp';
+
+	const cardPersonName = document.createElement('div');
+	cardPersonName.className = 'mdl-card__title mdl-card--expand';
+	cardWrapper.appendChild(cardPersonName);
+
+	const personDataWrapper = document.createElement('span');
+	personDataWrapper.className = 'mdl-list__item-primary-content';
+	cardPersonName.appendChild(personDataWrapper);
+
+	const personIcon = document.createElement('i');
+	personIcon.className = 'material-icons mdl-list__item-avatar';
+	personIcon.textContent =  'person';
+	personDataWrapper.appendChild(personIcon);
+
+	const personName = document.createElement('h4');
+	personName.className = 'pwa-card-name__name-header';
+	personName.textContent = 'Emlen Lafuente';
+	personDataWrapper.appendChild(personName);
+
+	const personOtherData = document.createElement('div');
+	personOtherData.className = 'mdl-card__supporting-text';
+	cardWrapper.appendChild(personOtherData);
+
+	const spanLabelArray = [];
+	const spanDataArray = [];
+
+	for (let index = 0; index < 3; index++) {
+		const seperatingDiv = document.createElement('div');
+		seperatingDiv.className = 'pwa-card-other-data-wrapper';
+		personOtherData.appendChild(seperatingDiv);
+	
+		const personOtherDataCardLabel = document.createElement('span');
+		personOtherDataCardLabel.className = 'pwa-card-label';
+		seperatingDiv.appendChild(personOtherDataCardLabel);
+
+		const personOtherDataCardData = document.createElement('span');
+		personOtherDataCardData.className = 'mdl-list__item-sub-title';
+		seperatingDiv.appendChild(personOtherDataCardData);
+
+		spanDataArray.push(personOtherDataCardData);
+		spanLabelArray.push(personOtherDataCardLabel);
+	}
+
+	spanLabelArray[0].textContent = 'email: ';
+	spanLabelArray[1].textContent = 'adress: ';
+	spanLabelArray[2].textContent = 'bitcoin: ';
+
+	spanDataArray[0].textContent = 'elafuente1@scribd.com:';
+	spanDataArray[1].textContent = '1184 Dunning Street:';
+	spanDataArray[2].textContent = '1EuupijjV7GStvHA8Ee6dLhEo4zbpWNUfh:';
+
+	const seperatingDivForImage = document.createElement('div');
+	seperatingDivForImage.className = 'pwa-card-other-data-wrapper';
+	personOtherData.appendChild(seperatingDivForImage);
+
+	const personOtherDataImage = document.createElement('img');
+	personOtherDataImage.setAttribute('src', '../src/images/brooke-lark-229136.jpg');
+	personOtherDataImage.setAttribute('alt', 'xyz');
+	seperatingDivForImage.appendChild(personOtherDataImage);
+
+	const personDataOtherButtonWrapper = document.createElement('div');
+	personDataOtherButtonWrapper.className = 'mdl-card__actions mdl-card--border';
+	cardWrapper.appendChild(personDataOtherButtonWrapper);
+
+	const personDataOtherButton = document.createElement('button');
+	personDataOtherButton.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--colored';
+	personDataOtherButton.textContent = 'View';
+	personDataOtherButtonWrapper.appendChild(personDataOtherButton);
+
+	pwaContactsList.appendChild(cardWrapper);
+
+	}	
+};
+
+createContact();
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 
---
+++ webpack.config.js
@@ -0,0 +1,93 @@
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+const OptimizeCSSAssets = require('optimize-css-assets-webpack-plugin');
+
+let config = {
+    entry: [
+        path.resolve(__dirname, 'public/src/index.js')
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].bundle.js'
+    },
+    watch: true,
+    resolve: { // These options change how modules are resolved
+        extensions: ['.js', '.jsx', '.json', '.scss', '.css', '.jpeg', '.jpg', '.gif', '.png'], // Automatically resolve certain extensions
+        alias: { // Create aliases
+            images: path.resolve(__dirname, 'public/src/images/')  // src/assets/images alias
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ['css-hot-loader'].concat(ExtractTextWebpackPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'postcss-loader']
+                }))
+            },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader" // creates style nodes from JS strings
+                }, {
+                    loader: "css-loader" // translates CSS into CommonJS
+                }, {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }, {
+                    loader: "postcss-loader" // creates style nodes from JS strings
+                }]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                loaders: ['file-loader?context=src/assets/images/&name=images/[path][name].[ext]', {  // images loader
+                    loader: 'image-webpack-loader',
+                    query: {
+                        mozjpeg: {
+                            progressive: true,
+                        },
+                        gifsicle: {
+                            interlaced: false,
+                        },
+                        optipng: {
+                            optimizationLevel: 4,
+                        },
+                        pngquant: {
+                            quality: '75-90',
+                            speed: 3,
+                        },
+                    },
+                }],
+                exclude: /node_modules/,
+                include: __dirname,
+            },
+        ] // end rules
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin(),
+        new ExtractTextWebpackPlugin('styles.css')
+    ],
+    devServer: {
+        port: 3002,
+        contentBase: path.resolve(__dirname, 'public'),
+        historyApiFallback: true,
+        inline: true,
+        open: true
+    },
+    devtool: "eval-source-map"
+}
+
+module.exports = config;
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.plugins.push(
+        new webpack.optimize.UglifyJsPlugin(), // call the uglify plugin
+        new OptimizeCSSAssets()
+    );
+}
