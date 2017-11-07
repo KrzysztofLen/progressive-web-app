1b81a9bb1af07c617234b7c99f3c54e35e099781
Krzysiek <hakkai660@gmail.com>
2017-11-07 20:46:23 +0100

    Change createContact function

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -54,81 +54,38 @@ const clearContact = () => {
 };
 
 const createContact = (data) => {
-
-	const cardWrapper = document.createElement('div');
-	cardWrapper.className = 'demo-card-square mdl-card mdl-shadow--2dp';
-
-	const cardPersonName = document.createElement('div');
-	cardPersonName.className = 'mdl-card__title mdl-card--expand';
-	cardWrapper.appendChild(cardPersonName);
-
-	const personDataWrapper = document.createElement('span');
-	personDataWrapper.className = 'mdl-list__item-primary-content';
-	cardPersonName.appendChild(personDataWrapper);
-
-	const personIcon = document.createElement('i');
-	personIcon.className = 'material-icons mdl-list__item-avatar';
-	personIcon.textContent =  'person';
-	personDataWrapper.appendChild(personIcon);
-
-	const personName = document.createElement('h4');
-	personName.className = 'pwa-card-name__name-header';
-	personName.textContent = data.name;
-	personDataWrapper.appendChild(personName);
-
-	const personOtherData = document.createElement('div');
-	personOtherData.className = 'mdl-card__supporting-text';
-	cardWrapper.appendChild(personOtherData);
-
-	const spanLabelArray = [];
-	const spanDataArray = [];
-
-	for (let index = 0; index < 3; index++) {
-		const seperatingDiv = document.createElement('div');
-		seperatingDiv.className = 'pwa-card-other-data-wrapper';
-		personOtherData.appendChild(seperatingDiv);
-
-		const personOtherDataCardLabel = document.createElement('span');
-		personOtherDataCardLabel.className = 'pwa-card-label';
-		seperatingDiv.appendChild(personOtherDataCardLabel);
-
-		const personOtherDataCardData = document.createElement('span');
-		personOtherDataCardData.className = 'mdl-list__item-sub-title';
-		seperatingDiv.appendChild(personOtherDataCardData);
-
-		spanDataArray.push(personOtherDataCardData);
-		spanLabelArray.push(personOtherDataCardLabel);
-	}
-
-	spanLabelArray[ 1 ].textContent = 'adress: ';
-	spanLabelArray[ 2 ].textContent = 'bitcoin: ';
-	spanLabelArray[ 0 ].textContent = 'email: ';
-
-	spanDataArray[ 0 ].textContent = 	data.email;
-	spanDataArray[ 1 ].textContent = 	data.adress;
-	spanDataArray[ 2 ].textContent = 	data.bitcoin;
-
-	const seperatingDivForImage = document.createElement('div');
-	seperatingDivForImage.className = 'pwa-card-other-data-wrapper';
-	personOtherData.appendChild(seperatingDivForImage);
-
-	const personOtherDataImage = document.createElement('img');
-	personOtherDataImage.setAttribute('src', data.image);
-	personOtherDataImage.setAttribute('alt', 'xyz');
-	seperatingDivForImage.appendChild(personOtherDataImage);
-
-	const personDataOtherButtonWrapper = document.createElement('div');
-	personDataOtherButtonWrapper.className = 'mdl-card__actions mdl-card--border';
-	cardWrapper.appendChild(personDataOtherButtonWrapper);
-
-	const personDataOtherButton = document.createElement('button');
-	personDataOtherButton.className = 'mdl-button mdl-js-button mdl-button--raised mdl-button--colored';
-	personDataOtherButton.textContent = 'View';
-	personDataOtherButtonWrapper.appendChild(personDataOtherButton);
-
-	pwaContactsList.appendChild(cardWrapper);
-
-};
+	const template = `
+		<div class="demo-card-square mdl-card mdl-shadow--2dp">
+			<div class="mdl-card__title mdl-card--expand">
+				<span class="mdl-list__item-primary-content">
+					<i class="material-icons mdl-list__item-avatar">person</i>
+					<h4 class="pwa-card-name__name-header">${data.name}</h4>
+				</span>
+			</div>
+			<div class="mdl-card__supporting-text">
+				<div class="pwa-card-other-data-wrapper">
+					<span class="pwa-card-label">email: </span>
+					<span class="mdl-list__item-sub-title">${data.email}</span>
+				</div>
+				<div class="pwa-card-other-data-wrapper">
+					<span class="pwa-card-label">adress: </span>
+					<span class="mdl-list__item-sub-title">${data.adress}</span>
+				</div>
+				<div class="pwa-card-other-data-wrapper">
+					<span class="pwa-card-label">bitcoin: </span>
+					<span class="mdl-list__item-sub-title">${data.bitcoin}</span>
+				</div>
+				<div class="pwa-card-other-data-wrapper">
+					<img src="${data.image}" alt="xyz">
+				</div>
+			</div>
+			<div class="mdl-card__actions mdl-card--border">
+				<button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">View</button>
+			</div>
+		</div>
+	`
+	pwaContactsList.innerHTML += template;
+}
 
 shareImageButton.addEventListener('click', openCreatePostModal);
 
@@ -137,7 +94,7 @@ closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
 const updateUI = (data) => {
 	clearContact();
 	for (let index = 0; index < data.length; index++) {
-		createContact(data[ index ]);
+		createContact(data[index]);
 	}
 };
 
@@ -155,7 +112,7 @@ fetch(url)
 		const dataArray = [];
 
 		for (const key in data) {
-			dataArray.push(data[ key ]);
+			dataArray.push(data[key]);
 		}
 
 		updateUI(dataArray);
@@ -181,10 +138,10 @@ const sendData = () => {
 		},
 		body: JSON.stringify({
 			id: new Date().toISOString(),
-			name:	name.value,
-			email:	email.value,
-			adress:	adress.value,
-			bitcoin:	bitcoin.value,
+			name: name.value,
+			email: email.value,
+			adress: adress.value,
+			bitcoin: bitcoin.value,
 			image: 'https://firebasestorage.googleapis.com/v0/b/pwa-app-72fbb.appspot.com/o/brooke-lark-229136.jpg?alt=media&token=922238cc-7795-4316-ab1e-b32864c39f54'
 		})
 	})
@@ -209,20 +166,20 @@ form.addEventListener('submit', (event) => {
 
 				const contact = {
 					id: new Date().toISOString(),
-					name:	name.value,
-					email:	email.value,
-					adress:	adress.value,
-					bitcoin:	bitcoin.value
+					name: name.value,
+					email: email.value,
+					adress: adress.value,
+					bitcoin: bitcoin.value
 				};
 
 				setAllData('sync-contacts', contact)
 					.then(() => {
 						return sw.sync.register('sync-new-contact');
 					})
-				// showing material message
+					// showing material message
 					.then(() => {
 						const snackbarContainer = document.querySelector('#confirmation-toast');
-						const data = {	message: 'Your Post was saved for syncing!'	};
+						const data = {message: 'Your Post was saved for syncing!'};
 						snackbarContainer.MaterialSnackbar.showSnackbar(data);
 					})
 					.catch((err) => {
--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const config = {
 	],
 	output: {
 		path: path.resolve(__dirname, 'public'),
-		filename: '[name].example.js'
+		filename: '[name].bundle.js'
 	},
 	watch: true,
 	resolve: { // These options change how modules are resolved
